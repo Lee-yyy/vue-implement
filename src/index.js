80d0628af2b4c7a66e0373873f51e9ebf086e402
Lee-yyy <72646109+Lee-yyy@users.noreply.github.com>
2020-10-21 12:43:59 +0800

    模板编译

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import { initMixin } from "./init";
+import { lifecycleMixin } from "./lifecycle";
+import { renderMixin } from "./render";
 
 function Vue(options) {
   this._init(options); // 当用户new Vue时 就调用init方法进行vue的初始方法
 }
 
 initMixin(Vue);
+lifecycleMixin(Vue); // 扩展_update方法
+renderMixin(Vue); // 扩展_render方法
 export default Vue
 
 
